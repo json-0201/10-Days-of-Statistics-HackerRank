a9c5af6f630a4e639088f29d09c9c0b869040840
json-0201 <106159207+json-0201@users.noreply.github.com>
2022-12-02 15:43:47 -0800

    Create day_5-3.py

---
+++ day_5-3.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+#from scipy.stats import norm as n
+
+#p1 = n(20,2).cdf(19.5)
+#p2 = n(20,2).cdf(22)-n(20,2).cdf(20)
+
+# print("%.3f" %p1) => 0.401
+# print("%.3f" %p2) => 0.341
+
+print(0.401,"\n",0.341)
